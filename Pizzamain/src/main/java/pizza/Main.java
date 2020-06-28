ad0724ad3b5d172cdabb0dd22e812024b023e18e
VasylPolukhovych <nalench7@gmail.com>
2020-06-28 16:15:55 +0300

    Spring xml

--- Pizzamain/src/main/java/pizza/Main.java
+++ Pizzamain/src/main/java/pizza/Main.java
@@ -1,6 +1,10 @@
 package pizza;
 
 import org.postgresql.ds.PGSimpleDataSource;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import pizza.dao.CurrentMenuDAO;
 import pizza.dto.CookedDish;
 import pizza.input.InputData;
@@ -13,33 +17,36 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class Main {
-    private static Reports reports = new Reports();
-    private static InputData inputData = new InputDataOfOrder();
+    //private static InputData inputData = new InputDataOfOrder();
     private static CurrentMenuDAO currentMenu;
 
     public static void main(String[] args) throws Exception {
 
-        PGSimpleDataSource source = new PGSimpleDataSource();
-        source.setServerNames(new String[]{"localhost"});
-        source.setDatabaseName("Testdb");
-        source.setUser("postgres");
-        source.setPassword("admin");
 
         Connection conn = null;
+
+        String confFile = "applicationContext.xml";
+        ApplicationContext context = new ClassPathXmlApplicationContext(confFile);
+        PGSimpleDataSource ds = (PGSimpleDataSource) context.getBean("dataSource");
+        InputData input = (InputData) context.getBean("input");
+        Reports report = (Reports) context.getBean("reports");
+
         try {
-            conn = source.getConnection();
+
+            conn = ds.getConnection();
             currentMenu = new CurrentMenuDAO(conn);
             List<CookedDish> menu = currentMenu.getCurrentMenu();
-            reports.printMenu(menu);
+
+            report.printMenu(menu);
             boolean isContinue = true;
             while (isContinue) {
-                ServeClient serveClient = new ServeClient(inputData, menu, conn);
+               ServeClient serveClient = (ServeClient) context.getBean("serveClient", input, menu, conn);
                 serveClient.getNewOrder();
-                isContinue = inputData.isInputEnd("Do you want to continue ?");
+                isContinue = input.isInputEnd("Do you want to continue ?");
             }
-            reports.printDishs(currentMenu.getDishes(), "All cooked dishes");
-            reports.printDishs(currentMenu.getDishes(), "All spoiled dishes");
-            reports.printOrder(2, null);
+            report.printDishs(currentMenu.getDishes(), "All cooked dishes");
+            report.printDishs(currentMenu.getDishes(), "All spoiled dishes");
+            report.printOrder(2, null);
             conn.close();
 
         } catch (SQLException e) {
